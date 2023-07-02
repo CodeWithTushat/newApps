89a5c978e758d059a1d5f9a5d2a7e8dca2b21123
CodeWithTushat <codewithtushar840@gmail.com>
2023-07-02 20:23:13 +0530

    commit

--- comps/src/app/collections/collections-routing.module.ts
+++ comps/src/app/collections/collections-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponentComponent } from './collections-home-component/collections-home-component.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CollectionsHomeComponentComponent
+    component: CollectionsHomeComponentComponent,
+    children: [
+      {
+        path:'',
+        component: BiographyComponent
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent
+      }
+    ]
   }
 ];
 
